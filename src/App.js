0297853a3ba96c672768fc0c5b294b7a2b319da5
Emin Kumru <eminkumru135@outlook.com>
2023-02-26 14:59:04 +0300

    link tag

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import AboutUs from "./Components/AboutUs";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/aboutUs" element={<AboutUs />} />
